e18fae0a12e545b0c596157b74101df3a5d0f188
Julian Schäfer <info@julian-schaefer.net>
2020-04-12 21:55:05 +0200

    fixed tag filtering in notes overview

--- pages/notes/index.vue
+++ pages/notes/index.vue
@@ -18,20 +18,34 @@ export default {
     items() {
       return this.$store.getters.getItems()
     },
+    itemsNotTrashed() {
+      return this.items.filter(
+        (item) => !item.tags.some((tag) => tag.text === 'Trash')
+      )
+    },
+    itemsTrashed() {
+      return this.items.filter((item) =>
+        item.tags.some((tag) => tag.text === 'Trash')
+      )
+    },
     filteredItems() {
-      return this.tag
-        ? this.items.filter((item) =>
-            item.tags
-              ? item.tags.some(
-                  (tag) => tag.text.toLowerCase() === this.tag.toLowerCase()
-                )
-              : false
-          )
-        : this.items.filter((item) =>
-            item.tags
-              ? item.tags.every((tag) => tag.text.toLowerCase() !== 'trash')
-              : false
-          )
+      if (this.tag === 'trash') {
+        return this.itemsTrashed
+      } else if (this.tag) {
+        return this.getItemsNotTrashedFromTag(this.tag)
+      } else {
+        return this.itemsNotTrashed
+      }
+    }
+  },
+
+  methods: {
+    getItemsNotTrashedFromTag(currentTag) {
+      return this.itemsNotTrashed.filter((item) =>
+        item.tags.some(
+          (tag) => tag.text.toLowerCase() === currentTag.toLowerCase()
+        )
+      )
     }
   }
 }
