0b7151d53a3ce159ae5465aab5d261018d3fa4d7
Julian Schäfer <info@julian-schaefer.net>
2021-04-03 23:17:38 +0200

    add giphy easter egg

--- components/PrivyNoteOverview.vue
+++ components/PrivyNoteOverview.vue
@@ -3,6 +3,7 @@
     <StaggeredTransition
       class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4vw lg:gap-2vw pb-4vw md:pb-0 items-center mb-6"
     >
+      <PrivyGiphy key="giphy" />
       <PrivyNoteTeaser
         v-for="(note, key) in notes"
         :key="key"
@@ -14,11 +15,13 @@
 </template>
 
 <script>
+import PrivyGiphy from '@/components/PrivyGiphy'
 import PrivyNoteTeaser from '@/components/PrivyNoteTeaser'
 import StaggeredTransition from '@/components/_StaggeredTransition'
 
 export default {
   components: {
+    PrivyGiphy,
     PrivyNoteTeaser,
     StaggeredTransition,
   },
