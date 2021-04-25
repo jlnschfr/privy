3267346b8792290e3fbca5351454029f7b37e552
Julian Schäfer <info@julian-schaefer.net>
2021-04-25 20:15:46 +0200

    remove lazy hydration because of bugs, remove firebase plugin

--- components/PrivyNoteOverview.vue
+++ components/PrivyNoteOverview.vue
@@ -3,9 +3,12 @@
     <StaggeredTransition
       class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4vw lg:gap-2vw pb-4vw md:pb-0 items-center mb-6"
     >
-      <LazyHydrate v-for="(note, key) in notes" :key="key" when-visible>
-        <PrivyNoteTeaser :note="note" :data-index="key" />
-      </LazyHydrate>
+      <PrivyNoteTeaser
+        v-for="(note, key) in notes"
+        :key="key"
+        :note="note"
+        :data-index="key"
+      />
     </StaggeredTransition>
   </div>
 </template>
@@ -13,7 +16,6 @@
 <script>
 export default {
   components: {
-    LazyHydrate: () => import('vue-lazy-hydration'),
     PrivyNoteTeaser: () => import('@/components/PrivyNoteTeaser'),
     StaggeredTransition: () => import('@/components/_StaggeredTransition'),
   },
