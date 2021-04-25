4eb66d823765bd5bdbd73b42dce45c45c0856a93
Julian Schäfer <info@julian-schaefer.net>
2021-04-25 19:52:00 +0200

    fix bug for lazy hydration

--- components/PrivyNoteOverview.vue
+++ components/PrivyNoteOverview.vue
@@ -3,13 +3,8 @@
     <StaggeredTransition
       class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4vw lg:gap-2vw pb-4vw md:pb-0 items-center mb-6"
     >
-      <LazyHydrate when-visible>
-        <PrivyNoteTeaser
-          v-for="(note, key) in notes"
-          :key="key"
-          :note="note"
-          :data-index="key"
-        />
+      <LazyHydrate v-for="(note, key) in notes" :key="key" when-visible>
+        <PrivyNoteTeaser :note="note" :data-index="key" />
       </LazyHydrate>
     </StaggeredTransition>
   </div>
