23f01dfbafb2c3ec2e44635698d022bb1c77d5f1
Julian Schäfer <info@julian-schaefer.net>
2021-04-26 13:57:35 +0200

    remove transition and add lazy hydrate

--- components/PrivyNoteOverview.vue
+++ components/PrivyNoteOverview.vue
@@ -1,23 +1,20 @@
 <template>
-  <div class="relative">
-    <StaggeredTransition
-      class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4vw lg:gap-2vw pb-4vw md:pb-0 items-center mb-6"
-    >
-      <PrivyNoteTeaser
-        v-for="(note, key) in notes"
-        :key="key"
-        :note="note"
-        :data-index="key"
-      />
-    </StaggeredTransition>
+  <div
+    class="relative grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4vw lg:gap-2vw pb-4vw md:pb-0 items-center mb-6"
+  >
+    <LazyHydrate v-for="(note, key) in notes" :key="key" when-visible>
+      <PrivyNoteTeaser :note="note" />
+    </LazyHydrate>
   </div>
 </template>
 
 <script>
+import LazyHydrate from 'vue-lazy-hydration'
+
 export default {
   components: {
+    LazyHydrate,
     PrivyNoteTeaser: () => import('@/components/PrivyNoteTeaser'),
-    StaggeredTransition: () => import('@/components/_StaggeredTransition'),
   },
 
   props: {
